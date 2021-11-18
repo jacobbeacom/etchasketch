d1684ef843da6ce78f76f5b51449903ffada6329
jacobbeacom <jdbeacom97@comcast.net>
2021-11-17 19:45:46 -0600

    Debugged and corrected Javascript logic for the clear drawing and change size buttons. Clear button now appropriately resets the grid to transparent (the default value). (did nothing or cleared every html element previously) Change size button now prompts the user for a new grid size, then clears the grid before creating new squares and re-enabling the drawing function. This fixed an issue that had previously been occuring where a size change would not allow you to color in the newly created divs.

--- scripts.js
+++ scripts.js
@@ -12,23 +12,25 @@ let myFunction = function(){
     this.style.setProperty('background-color', 'black');
 }
 
-
-
-function makeDivs() {
-for (i=0; i < totalDivs; i++) {
-   div = document.createElement('div');
-   div.id = i;
-   container.appendChild(div);
-}}
-
-makeDivs();
-
-document.getElementById('container').childNodes.forEach(function(element) {
+let drawFunction = function (){
+    document.getElementById('container').childNodes.forEach(function(element) {
     element.addEventListener('mouseover', myFunction);
 
-});  
+})}; 
 
-document.getElementById('clear').addEventListener('click', () => {
+function makeDivs() {
+    totalDivs = newSize * newSize;
+    for (i=0; i < totalDivs; i++) {
+        div = document.createElement('div');
+        div.id = i;
+        div.style.outline = "thin solid black";
+        container.appendChild(div);
+    }};
+
+makeDivs();
+drawFunction();
+
+function clearFunction() {
     let clearDiv = document.getElementById('container').childNodes;
         for (i=0; i<clearDiv.length; i++) {
             if (clearDiv[i].nodeName.toLowerCase() == 'div') {
@@ -36,20 +38,35 @@ document.getElementById('clear').addEventListener('click', () => {
             }
         }
     
-})
+}
 
+
+
+
+document.getElementById('clear').addEventListener('click', clearFunction)
+
+    
+
+ 
 document.getElementById('changeSize').addEventListener('click', () => {
     newSize = prompt("What size grid would you like? (Grid will stay a square, only enter one value");
         if (newSize > 64){
             alert("Sorry, max value is 64");
     }   else {
+            clearFunction();
             container.style.setProperty('grid-template-rows', 'repeat(' + newSize + ', 1fr)');
             container.style.setProperty('grid-template-columns', 'repeat(' + newSize + ', 1fr)');
             totalDivs = newSize * newSize;
             makeDivs();
+            drawFunction();
 
 
 }});
+
+
+
+
+
 
 
             
